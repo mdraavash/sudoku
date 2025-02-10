c794c8c00010d1c21854cd44d60fcc19c39fd91e
mdraavash <aavashmdr02@gmail.com>
2025-02-10 21:29:44 +0545

    better display

--- user.cpp
+++ user.cpp
@@ -28,13 +28,16 @@ public:
 
     vector<vector<char>> user_board() {
         vector<vector<char>> board(9, vector<char>(9, '.'));
-        show_board_layout();
         cout << "\nEnter the position at which you want to enter the value along with the value:" << endl;
         cout << "To end the adding process, input (0 0 0)" << endl;
         bool getinput = true;
         size_t x, y;
         char val;
         while (getinput) {
+            system("cls");
+            show_board_layout();
+            cout<<"\n\n\nCurrent table layout: \n";
+            print_board(board);
             cout << "\nEnter row, column, and value (e.g., 3 5 7): ";
             cin >> x >> y >> val;
             if (x == 0 && y == 0 && val == '0') {
@@ -43,7 +46,8 @@ public:
                 break;
             }
             if (x < 1 || x > 9 || y < 1 || y > 9 || val < '1' || val > '9') {
-                cout << "Invalid input! Please enter a valid row (1-9), column (1-9), and digit (1-9)." << endl;
+                cout << "Invalid input! Please enter a valid row (1-9), column (1-9), and digit (1-9). \n press any button to continue! " << endl;
+                cin>>x;
                 continue;
             }
             board[x - 1][y - 1] = val;
@@ -55,13 +59,13 @@ public:
 
     void print_board(const vector<vector<char>>& board) {
         cout << "\nSudoku Board:\n";
-        cout << "  -----------------------------------------------\n";
+        cout << "  -----------------------------------------------------\n";
         for (size_t row = 0; row < 9; ++row) {
             cout << " | ";
             for (size_t col = 0; col < 9; ++col) {
                 cout << board[row][col] << "  |  ";
             }
-            cout << "\n  --------------------------------------------\n";
+            cout << "\n  -----------------------------------------------------\n";
         }
     }
 };
